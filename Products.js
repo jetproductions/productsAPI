bdc19ffa605e45fe4f2ef810f94e8fb0e1cbbb9b
James Kelley <james.a.kelley.mars@gmail.com>
2020-02-13 09:44:27 -0700

    Create dotenv configuration

--- Products.js
+++ Products.js
@@ -2,9 +2,10 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 const bodyParser = require ('body-parser');
+require ('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(port, ()=> console.log(`Listening on port ${port}!`))
+app.listen(port, ()=> console.log(`Listening on port ${port}!`));
 
