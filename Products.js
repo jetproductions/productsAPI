4e6ffe344e92f8564c23d034da6223d35321905b
James Kelley <james.a.kelley.mars@gmail.com>
2020-02-12 17:23:41 -0700

    Setup more dependencies, write basic server code

--- Products.js
+++ Products.js
@@ -1,2 +1,10 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+const bodyParser = require ('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, ()=> console.log(`Listening on port ${port}!`))
 
