02c2c9dca630f11516a47611856cd37c9ca3ab96
James Kelley <james.a.kelley.mars@gmail.com>
2020-02-18 12:27:55 -0700

    Establish correct format for routes and test for reply

---
+++ db/database.js
@@ -0,0 +1,25 @@
+const {Pool} = require ('pg');
+const Client = require ('pg-native');
+require ('dotenv').config();
+
+const connectString = process.env.SQL_STRING;
+
+const client = new Pool({
+    connectionString : connectString,
+});
+
+// const connectString = process.env.SQL_STRING;
+
+// client.connectSync(connectString,(err)=>{
+//     if (err) console.error(err, err.stack);
+//     console.log('Connected to DB...')
+// });
+
+
+// let connection = () => {
+//     return client.connect(connectString,(err)=>{
+//     if (err) console.error(err, err.stack);
+//     else {console.log('Connected to DB...')};}
+// )};
+
+module.exports = client;
--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var router = express.Router()
+var client = require ('./db/database');
+require ('dotenv').config();
 
 
 // ROUTES FOR API CALLS
@@ -9,22 +11,109 @@ router.get('/', (req,res)=>{
     res.send('Did you need a longer URL?');
 })
 
-// /products/list
+
+// /PRODUCTS/LIST
+// Returns array list of ALL products as objects with keys:
+// 		"id":  NUMBER 
+// 		"name":  STRING
+// 		"slogan":  STRING
+// 		"description": 	STRING
+// 		"category":  STRING
+// 		"default_price":  STRING
 router.get('/list', (req,res)=>{
-    res.send('PRODUCT LIST!');
+    let query = 'SELECT * FROM public.product WHERE id = $1';
+    let params = [1];
+client.query(query, params)
+    .then(reply => res.send(reply));
+    // res.send(results)
+    // res.send('PRODUCT LIST!');
 })
 
-//  /products/:product_id
-router.get('/:product_id', (req,res)=>{
+
+//  /PRODUCTS/:PRODUCT_ID
+// Returns an object representing a product - specific version of all above details, but also features.
+// {
+// 	"id": NUMBER
+// 	"name": STRING
+// 	"slogan": STRING
+// 	"description": STRING
+// 	"category": STRING
+// 	"default_price": STRING
+// 	"features": [
+//   		{
+// 			"feature": STRING
+// 			"value": STRING
+// 		},
+//   		{
+// 			"feature": STRING
+// 			"value": STRING
+// 		},
+// 	]
+// }
+router.get('/:product_id/', (req,res)=>{
+    // if (req.params.id !== )
     res.send(`PRODUCT ${req.params.product_id} INFO!`);
 })
 
-//  /products/:product_id/styles
+//  /PRODUCTS/:PRODUCT_ID/STYLES
+// Returns a list of styles under RESULTS, with stringified key at PRODUCT_ID
+
+// {
+// 	"product_id": STRING
+// 	"results": [
+//   	{
+// 			"style_id": NUMBER
+// 			"name": STRING
+// 			"original_price": STRING
+// 			"sale_price": STRING
+// 			"default?": BOOLEAN? (NUMBER)
+// 			"photos": [
+//   			{
+// 					"thumbnail_url": STRING
+// 					"url": STRING
+// 				},
+//   			{
+// 					"thumbnail_url": STRING	
+// 					"url": STRING
+// 				}
+// 			],
+// 		"skus": {
+// 			"XS": NUMBER
+// 			"S": NUMBER
+// 			"M": NUMBER
+// 			"L": NUMBER
+// 			"XL": NUMBER
+// 		}
+// 	},
+//   {
+// 		"style_id": NUMBER
+// 		"name":  STRING
+// 		"original_price": STRING
+// 		"sale_price": STRING
+// 		"default?": STRING
+// 		"photos": [
+//   			{
+// 					"thumbnail_url": STRING
+// 					"url": STRING
+//         }
+// 			],
+// 		"skus": {
+// 			"S": NUMBER
+// 			"XS": NUMBER
+// 			"M": NUMBER
+// 			"L": NUMBER
+// 			"XL": NUMBER
+// 			"XXL": NUMBER
+// 			}
+// 	},
+// }
 router.get('/:product_id/styles', (req,res)=>{
     res.send(`PRODUCT ${req.params.product_id} STYLES!`);
 })
 
 //  /products/:product_id/related
+// returns a list of related products as numbers in an array with format:
+// [ NUMBER, NUMBER, NUMBER, NUMBER ]
 router.get('/:product_id/related', (req,res)=>{
     res.send(`PRODUCT !`);
 })
